7f75677790f3e07da1d40452ea004a3c453e6862
zzx <piofcircle@gmail.com>
2017-06-04 16:04:24 +0800

    h2db web config

--- src/sample/config/Config.java
+++ src/sample/config/Config.java
@@ -1,7 +1,15 @@
 package sample.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Created by Administrator on 2017/5/12 0012.
@@ -13,5 +21,41 @@ import org.springframework.test.context.ContextConfiguration;
 @ImportResource("classpath:springContext-test.xml")
 public class Config {
 
+    @Bean
+    public DataSource dataSource() {
+
+        // no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
+                .addScript("db/sql/create-db.sql")
+                .addScript("db/sql/insert-data.sql")
+                .build();
+        return db;
+    }
+
+    //@Bean
+    public DataSource getDataSourcedataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("db/create-db.sql")
+                .addScript("db/data-db.sql")
+                .build();
+        return db;
+    }
+
+    @Bean(initMethod="start",destroyMethod="stop")
+    public org.h2.tools.Server h2WebConsonleServer () throws SQLException {
+        return org.h2.tools.Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8888");
+    }
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
 
 }
